5a44811b6c92d90ae9caf3ee20d7ea831b53f538
Adam Birds <adbirds@hotmail.co.uk>
2017-10-20 12:38:17 +0100

    Added verification for Zulip server

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-module.exports = Franz => Franz;
+module.exports = Franz => class Zulip extends Franz {
+    async validateUrl(url) {
+        const baseUrl = new window.URL(url);
+        try {
+            const resp = await window.fetch(`${baseUrl.origin}/api/v1/server_settings`, {
+                method: 'GET',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+            });
+            const data = await resp.json();
+
+            return Object.hasOwnProperty.call(data, 'realm_uri');
+        } catch (err) {
+            console.error(err);
+        }
+
+        return false;
+    }
+};
