9084d3b4a5ccbd1ea6cde5dcf146061ab94e9901
Adam Birds <adbirds@hotmail.co.uk>
2017-10-21 14:57:16 +0100

    Add unread message badge

--- webview.js
+++ webview.js
@@ -1,3 +1,7 @@
 module.exports = (Franz) => {
-
+    const getMessages = function getMessages() {
+        const allMessages = Math.round(document.querySelectorAll("#global_filters .home-link .count .value")[0].innerText);
+        Franz.setBadge(allMessages);
+    };
+    Franz.loop(getMessages);
 };
