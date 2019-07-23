e4883658c605ea60e956651a18974e0d497dc86c
Akash Nimare <akashnimare@users.noreply.github.com>
2019-07-23 21:00:51 +0530

    webview: Update selector for all message.
    The `home-link` class is removed in the web app's left sidebar so this needs to be changed.

--- webview.js
+++ webview.js
@@ -1,6 +1,6 @@
 module.exports = (Franz) => {
     const getMessages = function getMessages() {
-        const allMessages = Math.round(document.querySelectorAll("#global_filters .home-link .count .value")[0].innerText);
+        const allMessages = Math.round(document.querySelectorAll("#global_filters .top_left_all_messages .count .value")[0].innerText);
         Franz.setBadge(allMessages);
     };
     Franz.loop(getMessages);
